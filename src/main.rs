d80cb7f93d75ebd23de33590e0df09e116fe7b8d
Jakub Skoupy <xskoupy1@fi.muni.cz>
2024-03-20 01:19:42 +0100

    Basic template parsing

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::time::SystemTime;
 
 mod parse;
-use parse::preprocess_line;
+use parse::{parse_template, preprocess_line, Template};
 
 const DEFAULT_DATA: &str = "./data";
 const DEFAULT_LOG: &str = "log";
@@ -39,9 +39,16 @@ fn command_template(data: String) {}
 fn main() {
     // prepare_files(DEFAULT_DATA, DEFAULT_LOG, DEFAULT_TEMPLATES);
     let arbitrary_line = "This line [ Recursively repeats [ \\[this shit \\] ]2 ]3 \n".to_string();
+    // let arbitrary_line = "This line [ Recursively repeats [ this shit ]2 ]3 \n".to_string();
 
-    let result_line = preprocess_line(arbitrary_line.clone()).unwrap();
+    let result_line = preprocess_line(arbitrary_line.clone(), 2).unwrap();
     println!("SOURCE: {}\n\n", arbitrary_line);
     println!("{}", result_line);
+
+    let template = "\"Edge Pull\" {num}mm {num:weight}kg x{num} {str} [{str}]5";
+    let template_preprocessed = preprocess_line(template.to_string(), 2).unwrap();
+
+    let template_struct = parse_template(template_preprocessed);
+    println!("{}", template_struct.name);
     // loop {}
 }
--- src/parse.rs
+++ src/parse.rs
@@ -1,23 +1,23 @@
+use core::panic;
 use std::{iter::Peekable, str::Chars, time::SystemTime};
 
 fn parse_expression() {}
 fn parse_text() {}
 
-enum TemplateToken {
+pub enum TemplateToken {
     Text(String),
     Field(Field),
 }
 
-enum Field {
-    Num(u16, bool),
-    String(String, bool),
+pub enum Field {
+    Num(String, bool),
+    Str(String, bool),
 }
 
-struct Template {
-    name: String,
-    tokens: Vec<TemplateToken>,
-    repeat_last: bool,
-    timestamp: Option<SystemTime>,
+pub struct Template {
+    pub name: String,
+    pub tokens: Vec<TemplateToken>,
+    pub repeat_last: bool,
 }
 
 fn apply_repetition(line: &str, a: usize, b: usize, repetition: &str) -> String {
@@ -32,78 +32,192 @@ fn apply_repetition(line: &str, a: usize, b: usize, repetition: &str) -> String 
     return result;
 }
 
-pub fn preprocess_line(line: String) -> Option<String> {
+pub fn preprocess_line(line: String, mut depth: usize) -> Option<String> {
     let mut process = true;
     let mut line_next = line;
-    while process {
+
+    while process && depth > 0 {
         let (line, process) = preprocess_line_step(line_next);
+        depth -= 1;
         line_next = line;
 
-        if !process {
-            return Some(line_next);
+        if !process || depth == 0 {
+            return Some(remove_redundant_escapes(line_next));
         }
     }
     None
 }
-// Applies all multipliers
-pub fn preprocess_line_step(line: String) -> (String, bool) {
-    let mut index = None; // Stores indexes of '['
-    let mut level = 0;
-    let mut after_block = false;
-    let mut continue_processing = false;
 
-    let mut result = String::new();
-    let mut stack = String::new
-    let mut rep_count_str = String::new();
-    let mut escape = false;
+fn remove_redundant_escapes(line: String) -> String {
+    let mut escape = false; // For escaping escape
+    let mut result = "".to_string();
 
-    for (i, c) in line.chars().enumerate() {
+    for c in line.chars() {
         match c {
-            c if escape => {
-                result.push(c);
+            '\\' if escape => {
                 escape = false;
+                result.push(c);
             }
             '\\' => escape = true,
+            _ => result.push(c),
+        }
+    }
+    return result;
+}
+
+fn parse_field(line_iterator: &mut Peekable<Chars>) -> Field {
+    let mut str_iter = line_iterator.clone();
+    let match_str: String = str_iter.collect();
+
+    let end_index = match_str.find('}').unwrap();
+    let field_string: String = match_str.chars().take(end_index).collect();
+    _ = line_iterator.take(end_index);
+    let mut words = field_string.split(':');
+
+    let keywords_num = vec!["n", "num", "number"];
+    let keywords_num_optional = vec!["n..", "num..", "number.."];
+
+    let keywords_str = vec!["s", "str", "string"];
+    let keywords_str_optional = vec!["s..", "str..", "string.."];
+
+    let mut field = match words.next() {
+        None => panic!(),
+        Some(n) if keywords_num.contains(&n) => Field::Num("".to_string(), false),
+        Some(n) if keywords_num_optional.contains(&n) => Field::Num("".to_string(), true),
+        Some(n) if keywords_str.contains(&n) => Field::Str("".to_string(), false),
+        Some(n) if keywords_str_optional.contains(&n) => Field::Str("".to_string(), true),
+        x => {
+            println!("INCORRECT: {}", x.unwrap());
+            panic!();
+        }
+    };
+
+    match words.next() {
+        None => (),
+        Some(name) => match &mut field {
+            Field::Str(s, _) | Field::Num(s, _) => *s = name.to_string(),
+        },
+    }
+
+    field
+}
+
+pub fn parse_template(line: String) -> Template {
+    // NAME PARSING
+    //
+    let mut line_iterator = line.chars().peekable();
+
+    if line_iterator.next() != Some('"') {
+        panic!();
+    }
+    let mut name = "".to_string();
+
+    loop {
+        match line_iterator.peek() {
+            Some('"') => break,
+            Some(_) => name.push(line_iterator.next().unwrap()),
+            None => panic!(),
+        }
+    }
+
+    let mut result = Template {
+        name,
+        tokens: vec![],
+        repeat_last: false,
+    };
+
+    // Tokenize
+    //
+    let mut text = "".to_string();
+    let mut text_ref = &mut text;
+
+    loop {
+        match line_iterator.peek() {
+            // Parse field
+            Some('{') => {
+                result.tokens.push(TemplateToken::Text(text));
+                text = "".to_string();
+                text_ref = &mut text;
+
+                line_iterator.next();
+
+                result
+                    .tokens
+                    .push(TemplateToken::Field(parse_field(&mut line_iterator)));
+            }
+            // Text
+            Some(_) => text_ref.push(line_iterator.next().unwrap()),
+            None => break,
+        }
+    }
+    result
+}
+
+// Applies all multipliers
+fn preprocess_line_step(line: String) -> (String, bool) {
+    let mut level = 0; // nesting level
+    let mut after_block = false; // flag for applying multiplier
+    let mut continue_processing = false; // no deeper nesting
+
+    let mut result = String::new();
+    let mut stack = String::new();
+
+    let mut rep_count_str = String::new();
+
+    let mut escape = false;
+
+    for c in line.chars() {
+        let string_ref = match level {
+            0 => &mut result,
+            _ => &mut stack,
+        };
+
+        match c {
+            // ----------------------- escapes
+            c if escape => {
+                string_ref.push(c);
+                escape = false;
+            }
+            '\\' => {
+                // string_ref.push(c);
+                escape = true;
+            }
+
+            // ----------------------- nesting
             '[' => {
                 level += 1;
-
                 match level > 1 {
                     true => {
-                        result.push(c);
+                        string_ref.push(c);
                         continue_processing = true;
                     }
-                    false => index = Some(i),
+                    _ => (),
                 }
             }
             ']' => {
                 after_block = true;
-
-                match level > 1 {
-                    true => result.push(c),
-                    _ => (),
+                if level > 1 {
+                    string_ref.push(c);
                 }
-
                 level -= 1;
             }
+            // ------------------------ space after block (Applies multiplier)
             ' ' if after_block && level == 0 => {
-                let nested =
-                    apply_repetition(line.as_str(), index.unwrap(), i, rep_count_str.as_str());
-
-                result += nested.as_str();
+                for _ in 0..rep_count_str.parse().unwrap_or(1) {
+                    result += stack.as_str();
+                }
 
                 rep_count_str = "".to_string();
+                stack = "".to_string();
 
                 after_block = false;
-                index = None;
                 result.push(c);
             }
 
-            '1'..='9' if after_block && level == 0 => rep_count_str.push(c), // APPLY MULT (MAX 9 I guess)
-            // c if after_block && level == 1 => panic!(),                      // expected mult
-            c => result.push(c),
+            // ------------------------- multiplier (has to be after block on outmost level)
+            '1'..='9' if after_block && level == 0 => rep_count_str.push(c),
+            c => string_ref.push(c),
         }
     }
-
-    println!("{}", result);
     (result, continue_processing)
 }
